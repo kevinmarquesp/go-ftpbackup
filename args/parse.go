663469fa2c75b3eebae0c99f3c4896657bce87a6
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-05-05 12:18:54 -0300

    :bug: fix: now the sources copy mechanism is allocating the space necessary before copying

--- args/parse.go
+++ args/parse.go
@@ -19,13 +19,16 @@ func Parse() Args {
 	flag.StringVar(&argv.Password, "password", "", "FTP connection password.")
 
 	flag.Var(&sources, "src", "List of file paths that you want to copy.")
-
-	copy(argv.Sources, sources) // removes the String() and Set() methods of the custom flags list type
-
 	flag.StringVar(&argv.Target, "target", "", "Path to the directory where everything will be saved.")
 	flag.BoolVar(&argv.IsDryRun, "dry", false, "Just show the log information, without actually copying anything.")
 
 	flag.Parse()
+
+	// Copy the sources array before return to remove the String() and Set() methods.
+
+	argv.Sources = make([]string, len(sources))
+
+	copy(argv.Sources, sources)
 
 	return argv
 }
