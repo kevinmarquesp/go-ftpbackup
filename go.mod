9c1c98901cbe161707016bde6f6d9dedc387ff9d
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-05-05 21:29:26 -0300

    :construction: wip: listing all files from the FTP server's root

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go-ftpbackup
 
 go 1.22.1
+
+require github.com/jlaffaye/ftp v0.2.0
+
+require (
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+)
--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"go-ftpbackup/connect"
 	"log"
 	"os"
+	"time"
+
+	"github.com/jlaffaye/ftp"
 )
 
 func main() {
@@ -19,5 +22,37 @@ func main() {
 
 	addr := argv.Machine + connect.ConvertIntPortToString(argv.Port)
 
-	log.Println("- Address:", addr)
+	var conn *ftp.ServerConn
+
+	// Try until it works because it's meat to be ran in background by some other script.
+
+	for {
+		conn, err = ftp.Dial(addr)
+		if err == nil {
+			break
+		}
+
+		fmt.Println(err)
+		time.Sleep(time.Second * 3)
+	}
+
+	defer conn.Quit()
+
+	err = conn.Login(argv.Username, argv.Password)
+	if err != nil {
+		fmt.Println(err)
+
+		os.Exit(2)
+	}
+
+	entries, err := conn.List("/")
+	if err != nil {
+		fmt.Println(err)
+
+		os.Exit(3)
+	}
+
+	for _, entry := range entries {
+		log.Println(entry.Name)
+	}
 }
