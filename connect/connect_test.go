7c33e484d2b80c2dcfeef3c8e4e5a2df44e88080
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-05-10 17:31:18 -0300

    :white_check_mark: test: created an integration test to check the connection of an FTP server

--- connect/connect_test.go
+++ connect/connect_test.go
@@ -3,9 +3,28 @@ package connect
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetConnection(t *testing.T) {
+	t.Run("(Integration) Should connect to the FTP server without any errors.", func(t *testing.T) {
+		done := make(chan struct{})
+
+		go func() {
+			_, err := GetConnection("localhost:2121", "kevin", "kevin123")
+			assert.NoError(t, err)
+		}()
+
+		select {
+		case <-time.After(5 * time.Second):
+			t.Log("Connection timeout. Did you start the integration server?")
+
+		case <-done:
+			t.Log("Successfully connected to the FTP server.")
+		}
+	})
+
 	t.Run("Should keep trying for a non-existing server for three seconds", func(t *testing.T) {
 		done := make(chan struct{})
 
@@ -16,13 +35,10 @@ func TestGetConnection(t *testing.T) {
 
 		select {
 		case <-time.After(3 * time.Second):
-			// After at least 3 seconds, stop trying to connect
 			t.Log("Test ran for at least 3 seconds")
+
 		case <-done:
-			// If connection is successful, stop the timer
 			t.Error("GetConnection exited before 3 seconds")
 		}
 	})
-
-	// TODO: Add more tests with a dummy FTP local server.
 }
